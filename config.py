8ad42d79818d43c84da37a85087ff7df55492fbc
AlanValerdi <alan.valerdip@alumno.buap.mx>
2025-10-29 23:35:23 -0600

    config.py fixed, Services Folder created

--- config.py
+++ config.py
@@ -8,5 +8,8 @@ load_dotenv()
 gemini_api_key = os.getenv('GEMINI_API_KEY')
 telegram_token = os.getenv('TELEGRAM_TOKEN')
 
-# Setring Gemini client (provisional)
-client = genai.Client(api_key=gemini_api_key, vertexai=False)
+# Handling missing variables
+if not gemini_api_key:
+    raise ValueError("Error: missing env var -> GEMINI_API_KEY")
+if not telegram_token:
+    raise ValueError("Error: missing env var -> TELEGRAM_TOKEN")
